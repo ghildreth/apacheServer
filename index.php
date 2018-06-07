5a3c30c3de1e951026ab41626dea4bd03d33fbb9
ghildreth <gabrielantonhildreth@gmail.com>
2018-06-07 15:23:29 -0700

    create POST for name submission

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello World</title>
+    
+</head>
+    <form action="process.php" method="post">
+        Enter your name: <input name="name" type="text">
+        <input type="submit">
+    </form>
+
+<body>
+    
+</body>
+</html>
---
+++ process.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>Hello World</title>
+
+</head>
+<body>
+    <?php
+        $name = $_POST["name"];
+        echo "hello, " . $name;
+    ?>
+</body>
+</html>
